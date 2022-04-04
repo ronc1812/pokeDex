acd99ddc5459413821b91f3a5a582cca9e7083e6
Ron Cohen <ronc@moveoboost.com>
2022-04-04 15:21:05 +0300

    adding logo to header

---
+++ pokedex/src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg"
+        alt="pokemon"
+      />
+    </>
+  );
+};
+
+export default Header;
